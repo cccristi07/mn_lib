0717957d930ebaeb0671fde9f05761d6f1619849
klaus <cccristi07@gmail.com>
2016-06-30 15:54:37 +0300

    modificat cmmp

--- QR/cmmp.m
+++ QR/cmmp.m
@@ -1,13 +1,6 @@
 function x = cmmp(A,b)
 
 	% functia rezolva un sistem supradeterminat A*x = b cu m > n
-	%   x = _A*b=(A'A)^-1A'b; unde _A este pseudoinversa
-	%  _A = (R'Q'QR)^-1R'Q' = ( R'R)^-1R'Q'
-	% A e R^(m*n)
-	% b e R^(m)
-	% x e R^(n)
-	% Q e R^(m*m)
-	% R e R^(m*n)
 	
 	
 	[m,n] = size(A);
